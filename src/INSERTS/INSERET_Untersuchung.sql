940e3ec9099b801af17e7b76e515686de37ef74f
jurkup <kai.kuehl@outlook.de>
2021-01-19 12:11:46 +0100

    Update

---
+++ src/INSERTS/INSERET_Untersuchung.sql
@@ -0,0 +1,32 @@
+INSERT INTO untersuchung(patient, behandelter_arzt, entstehender_arztbericht, datum)
+VALUES (1, 4, 1, '2021-01-13');
+
+INSERT INTO untersuchung(patient, behandelter_arzt, entstehender_arztbericht, datum)
+VALUES (2, 5, 2, '2021-01-14');
+
+INSERT INTO untersuchung(patient, behandelter_arzt, entstehender_arztbericht, datum)
+VALUES (3, 10, 3, '2021-01-15');
+
+INSERT INTO untersuchung(patient, behandelter_arzt, entstehender_arztbericht, datum)
+VALUES (4, 10, 4, '2021-01-16');
+
+INSERT INTO untersuchung(patient, behandelter_arzt, entstehender_arztbericht, datum)
+VALUES (5, 10, 5, '2021-01-17');
+
+INSERT INTO untersuchung(patient, behandelter_arzt, entstehender_arztbericht, datum)
+VALUES (6, 10, 6, '2021-01-18');
+
+INSERT INTO untersuchung(patient, behandelter_arzt, entstehender_arztbericht, datum)
+VALUES (7, 10, 7, '2021-01-19');
+
+INSERT INTO untersuchung(patient, behandelter_arzt, entstehender_arztbericht, datum)
+VALUES (8, 10, 8, '2021-01-20');
+
+INSERT INTO untersuchung(patient, behandelter_arzt, entstehender_arztbericht, datum)
+VALUES (9, 2, 9, '2021-01-21');
+
+INSERT INTO untersuchung(patient, behandelter_arzt, entstehender_arztbericht, datum)
+VALUES (10, 2, 10, '2021-01-15');
+
+
+
