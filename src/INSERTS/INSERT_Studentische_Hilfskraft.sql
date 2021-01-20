e98030667e7f5111aba27ee37313eaa777f5f718
Lars B <battla@protonmail.com>
2021-01-20 15:22:25 +0100

    changed Student

--- src/INSERTS/INSERT_Studentische_Hilfskraft.sql
+++ src/INSERTS/INSERT_Studentische_Hilfskraft.sql
@@ -28,13 +28,6 @@ INSERT INTO Studenten_Informationen(MatrikelNR, Abschlussart, Abschlussjahr, Uni
 VALUES(1803, 'Bachelor', '2024-01-01', 'Universität Osnabrück');
 
 INSERT INTO angestellter(Vorname, Nachname, Titel, Strasse, Hausnummer, PLZ, Stellentyp, Gehalt, Station, Geburtsdatum, EMail)
-VALUES('Steffen', 'Kleinschwanz', NULL, 'Studentenallee', 1, 48529, 'Studentische_Hilfskräfte', 1, 3, '2000-03-11', 'SKleinschwanz@Krankenhaus.de');
-INSERT INTO Studentische_Hilfskraft(PersonalSSN, Ausbilder, Arbeitsstunden, Matrikelnummer)
-VALUES(LAST_INSERT_ID(), 6,30, 1804);
-INSERT INTO Studenten_Informationen(MatrikelNR, Abschlussart, Abschlussjahr, Universitaet)
-VALUES(1804, 'Bachelor', '2023-01-01', 'Universität Sylt');
-
-INSERT INTO angestellter(Vorname, Nachname, Titel, Strasse, Hausnummer, PLZ, Stellentyp, Gehalt, Station, Geburtsdatum, EMail)
 VALUES('Ingrid', 'Ingridson', NULL, 'Studentenallee', 1, 48529, 'Studentische_Hilfskräfte', 1, 3, '1996-11-22', 'IIngridson@Krankenhaus.de');
 INSERT INTO Studentische_Hilfskraft(PersonalSSN, Ausbilder, Arbeitsstunden, Matrikelnummer)
 VALUES(LAST_INSERT_ID(), 6,30, 1805);
--- src/QUERY/SELECT_SUM_Stationsgehalt.sql
+++
@@ -1,5 +0,0 @@
-USE krankenhaus;
-SELECT DISTINCT Station,
-        (SELECT sum(Gehalt) FROM angestellter WHERE angestellter.Station <= a.Station ) AS Stationsgehalt
-FROM angestellter AS a
-ORDER BY Station
